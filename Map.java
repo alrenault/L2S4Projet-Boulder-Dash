801ef76cf26aa43917ccf48ba0e2b39e8b0227c6
Alexis Renault <alexis.renault8@sfr.fr>
2017-02-07 08:56:21 +0100

    définition des attributs de map + map en forme de tableau 2D de caractères

--- Map.java
+++ Map.java
@@ -22,12 +22,32 @@ public class Map {
 		name = "Cave "+numMap;
 	}
 	
+	public int nbLigne(String map){
+		Scanner sc = new Scanner(map).useDelimiter("\n");
+		String ligne ="";
+		int i = 0;
+		if(sc.hasNext()){
+			ligne = sc.next();
+			while(!ligne.equals("[map]")){
+				if(sc.hasNext())
+					ligne = sc.next();
+			}
+			while(!ligne.equals("[/map]")){
+				i++;
+				if(sc.hasNext())
+					ligne = sc.next();
+			}
+		}
+		return i-1;
+		
+	}
+	
 	public String findMap(File f){
 		String map = "";
 		Scanner sc;
+		String ligne ="";
 		try{
 			sc = new Scanner(f);
-			String ligne ="";
 			while(sc.hasNextLine()){
 				ligne = sc.nextLine();
 				if(ligne.equals("Name="+name)){
@@ -45,6 +65,94 @@ public class Map {
 			System.out.println("Fichier non trouvé");
 		}
 		
+		Scanner sc1 = new Scanner (map).useDelimiter("=|\n");
+		boolean quit = false;
+		while(sc1.hasNext() && !quit){
+			ligne = sc1.next();
+			switch(ligne){
+				case "CaveDelay":
+				{
+					if(sc1.hasNext()){
+						ligne = sc1.next();
+					}
+					break;
+				}
+				case "CaveTime":
+				{
+					if(sc1.hasNext())
+						ligne = sc1.next();
+					maptime = Integer.parseInt(ligne);
+					break;
+				}
+				case "DiamondsRequired":
+				{
+					if(sc1.hasNext())
+						ligne = sc1.next();
+					diamondRec = Integer.parseInt(ligne);
+					break;
+				}
+				case "DiamondsValue":
+				{
+					if(sc1.hasNext())
+						ligne = sc1.next();
+					String[] diamond = ligne.split("\\s+");
+					diamondVal = Integer.parseInt(diamond[0]);
+					diamondBonus = Integer.parseInt(diamond[1]);
+					System.out.println(diamondVal);
+					System.out.println(diamondBonus);
+					break;
+				}
+				case "AmoebaTime":
+				{
+					if(sc1.hasNext())
+						ligne = sc1.next();
+					amoebaT = Integer.parseInt(ligne);
+					break;
+				}
+				case "MagicWallTime":
+				{
+					if(sc1.hasNext())
+						ligne = sc1.next();
+					magicWallT = Integer.parseInt(ligne);					break;
+				}
+				case "":
+				{
+					break;
+				}
+				case"[map]":
+				{
+					if(sc1.hasNext()){
+						ligne = sc1.next();
+					}
+					int compt = nbLigne(map);
+					char [] ligneCourante = ligne.toCharArray();
+					int nbColonne = ligneCourante.length;
+					tab = new char[compt][nbColonne];
+					
+					for(int i =0;i<compt;i++){
+						ligneCourante = ligne.toCharArray();
+						for(int j = 0;j<nbColonne;j++){
+							tab[i][j] = ligneCourante[j];
+							//System.out.print(tab[i][j]);
+						}
+						//System.out.println();;
+						if(sc1.hasNext())
+							ligne = sc1.next();
+					}
+						
+						quit = true;
+				}
+					
+				default:
+				{
+					if(sc1.hasNext()){
+						ligne = sc1.next();
+					}
+				}
+			}
+				
+				
+		}
 		return map;
 	}
 	
