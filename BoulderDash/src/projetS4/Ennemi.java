91a601785d6b8577cf70707f02990925de980aba
J-Lyy <J-Lyy@users.noreply.github.com>
2017-02-27 08:33:52 +0100

    Delete Ennemi.java

--- BoulderDash/src/projetS4/Ennemi.java
+++
@@ -1,8 +0,0 @@
-package projetS4;
-
-public abstract class Ennemi extends Entite implements Vivant{
-	public Ennemi(boolean traversable, boolean enJeu,
-			 char apparence, int posX, int posY) {
-		super(traversable, enJeu, apparence, posX, posY);
-	}
-}
