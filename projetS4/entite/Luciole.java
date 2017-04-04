012905ed95171c1451650ed7304fa1959d6144c2
Quentin Lévèque <Lyy.q1000@gmail.com>
2017-04-04 10:16:14 +0200

    Petit ajout des notes sur l'algo genetique et comment l'implanter

--- projetS4/entite/Luciole.java
+++ projetS4/entite/Luciole.java
@@ -18,4 +18,20 @@ public class Luciole extends Entite implements Deplacable, Disparaitre {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	/*Déplacement :
+		Prendre en paramètre la dernière direction pour le prochain déplacement :
+		-> Si on allait à droite, on continue à droite si possible
+		
+		
+		Creer 100 tableaux de taille 2 fois le nombre de pas
+		-> on les remplit de HAUT,BAS,DROITE...
+		-> on détermine le score de chacun des 100 tableaux
+		-> on sélectionne les 50 meilleures scores -> on se retrouve avec 50 tableaux
+		-> on en rajoute 50 avec les combinaisons entre les 50 meilleurs ancients tableaux
+		-> on boucle
+		-> on parametre la manière dont on arrete
+		-> 1000 iterations
+		
+	*/
 }
