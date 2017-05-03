8771b2a6e3f7bce0f13ba852b6645872bd547921
J-Lyy <J-Lyy@users.noreply.github.com>
2017-05-03 16:33:41 +0200

    Merge Luciole

--- projetS4/src/entite/Luciole.java
+++ projetS4/src/entite/Luciole.java
@@ -187,8 +187,7 @@ public class Luciole extends Entite implements Deplacable, Disparaitre, Ennemi {
 			}
 			if(!moteur.isaPerdu()){
 				//deplacement
-				carte[p.getX()][p.getY()] = MoteurJeu.espace;
-				MoteurJeu.espace.getPosition().add(p); //rajoute l'emplacement de la libellule
+				moteur.ajouterUnEspace(p); //rajoute l'emplacement de la libellule a Espace
 						
 				this.getPosition().remove(p); //enleve la pos actuelle de this
 				carte[x][y] = this; //fait pointer sur la nouvelle pos
